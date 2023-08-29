1e48d2fcaccf4a4b20000b4a991ac5f9f1a4f393
rc4l <talhastrophe@gmail.com>
2023-08-28 22:12:07 -0500

    Nodemon is no longer in the default start script, it now uses start:dev instead

---
+++ runner.sh
@@ -0,0 +1,18 @@
+# This is a bash script for linux environments to quickly pull docker images and run them. It is meant to be used within 
+# as a helper script to quickly wipe and reset the docker image. If you aren't using docker or hosting on a linux environment
+# you can ignore this file. And even if you did, it's just a helper file anyway.
+
+# Usage: sh runner.sh <version>
+# Example: sh runner.sh v0.0.47
+echo "Stop and remove all containers...";
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+echo "...done";
+
+echo "Remove all images...";
+docker rmi $(docker images -a -q)
+echo "...done";
+
+echo "Setting up the bot with version $1...";
+docker run -d -e DATABASE_URL='UPDATETHIS' -e DISCORD_BOT_TOKEN='UPDATETHISTOO' -e DISCORD_APPLICATION_ID='ANDTHIS' --restart unless-stopped registry.digitalocean.com/discordkarmabot/discordkarmabot:$1
+echo "...done";
